3d3f2b3f5190f2c9ff4d1af89f6dcc4bf03329bd
Mohammad Khan <qcc2016@yahoo.com>
2024-06-14 10:10:34 -0400

    new changes

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, setSelectedCategory, selectedCategory }) {
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category, index) => (
+        <button
+          key={index}
+          className={category === selectedCategory ? 'selected' : null}
+          onClick={() => setSelectedCategory(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
